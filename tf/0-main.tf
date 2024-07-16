cb22dcd188a5629d856897aca451d38bb066f12b
Mark Riabikin <markrules@ya.ru>
2024-07-16 23:52:15 +0300

    add todo

--- tf/0-main.tf
+++ tf/0-main.tf
@@ -1,7 +1,19 @@
-# resource "aws_vpc" "my_vpc" {
-#   cidr_block = "10.0.0.0/24"
-#   tags = merge(
-#       local.common_tags,
-#       { some_tag = "value" }
-#     )
-# }
+
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.0.0.0/24"
+  tags = merge(
+      local.common_tags,
+      { version = "1" }
+    )
+}
+
+# 2 public subnets
+# 2 private subnets
+# internet gw
+# nat
+# eip
+# route table
+# ECR registry
+# EKS
+# node pool
+# IAM role binding
