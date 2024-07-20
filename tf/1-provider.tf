412a440800904dd0c747811359f685988cafd6e5
markrin <12709600+markrin@users.noreply.github.com>
2024-07-21 00:17:41 +0300

    Update 1-provider.tf to use local

--- tf/1-provider.tf
+++ tf/1-provider.tf
@@ -23,41 +23,55 @@ terraform {
   }
   required_version = ">= 1.2"
   # COMMENT BEFORE FIRST APPLY AND UNCOMMENT AFTER
-  backend "s3" {
-    bucket         = "markr-test-assignment-tf-state"
-    key            = "terraform.tfstate"
-    dynamodb_table = "markr-terraform-state-locking"
-    encrypt        = true
-  }
+  #backend "s3" {
+  #  bucket         = "markr-test-assignment-tf-state"
+  #  key            = "terraform.tfstate"
+  #  dynamodb_table = "markr-terraform-state-locking"
+  #  encrypt        = true
+  #}
+  backend "local" {}
 }
 
-resource "aws_s3_bucket" "terraform_state" {
-  bucket        = "markr-test-assignment-tf-state"
-  force_destroy = true
-}
+#resource "aws_s3_bucket" "terraform_state" {
+#   bucket        = "markr-test-assignment-tf-state"
+#   force_destroy = true
+#   lifecycle {
+#     prevent_destroy = true
+#   }
+# }
 
-resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
-  bucket = aws_s3_bucket.terraform_state.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+# resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
+#   bucket = aws_s3_bucket.terraform_state.id
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+#   lifecycle {
+#     prevent_destroy = true
+#   }
+# }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_crypto_conf" {
-  bucket = aws_s3_bucket.terraform_state.bucket 
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
+# resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_crypto_conf" {
+#   bucket = aws_s3_bucket.terraform_state.bucket 
+#   rule {
+#     apply_server_side_encryption_by_default {
+#       sse_algorithm = "AES256"
+#     }
+#   }
+#   lifecycle {
+#     prevent_destroy = true
+#   }
+# }
 
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "markr-terraform-state-locking"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
+# resource "aws_dynamodb_table" "terraform_locks" {
+#   name         = "markr-terraform-state-locking"
+#   billing_mode = "PAY_PER_REQUEST"
+#   hash_key     = "LockID"
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
+#   lifecycle {
+#     prevent_destroy = true
+#   }
+# }
+
